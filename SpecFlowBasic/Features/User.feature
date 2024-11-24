8502c3ad08b8dbf72ff1c66868d5307d7eb338cf
dheerajambippi108 <dheerajambippi108@outlook.com>
2024-11-24 20:00:15 +0530

    working with dynamic test data using auto fixture

---
+++ SpecFlowBasic/Features/User.feature
@@ -0,0 +1,10 @@
+﻿Feature: User
+
+To test user functionality
+
+@User
+Scenario: Test scenario for user entry data
+	Given I enter random user data
+	Given I enter random user data
+	Given I enter random user data
+	
---
+++ SpecFlowBasic/StepDefinitions/UserStepDefinitions.cs
@@ -0,0 +1,27 @@
+using AutoFixture;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowBasic.StepDefinitions
+{
+    [Binding]
+    public class UserStepDefinitions
+    {
+        [Given(@"I enter random user data")]
+        public void GivenIEnterRandomUserData()
+        {
+            //var person = new Fixture().Create<User>();
+            var person = new Fixture().Build<User>().With(x => x.Email, "dhee@example.co.in").Create();
+            Console.WriteLine($"Name : {person.Name} | Email : {person.Email} | Address : {person.Address} | Phone : {person.Phone}");
+        }
+    }
+
+
+    public record User
+    {
+        public string Name { get; init; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
